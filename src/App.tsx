4d967fb73aeef0f3a2ee9628dd71dbc84b57e78a
axelsoul07 <axelsoul07@yahoo.co.jp>
2021-12-02 11:27:41 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import "./styles.css";
+import { UserCard } from "./componets/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
+
+export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFetchUser = () => getUsers();
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
